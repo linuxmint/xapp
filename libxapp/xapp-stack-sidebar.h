0e063b8aa31ddf84ff58f850dfd7bbbb547dbe5a
JosephMcc <mccullarjoseph@gmail.com>
2018-08-13 01:43:53 -0700

    Add an XAppStackSidebar widget (#45)
    This is mostly a modified GtkStackSidebar that allows adding an icon. The
    placement of the icon is controlled by setting the orientation.

---
+++ libxapp/xapp-stack-sidebar.h
@@ -0,0 +1,22 @@
+#ifndef _XAPP_STACK_SIDEBAR_H_
+#define _XAPP_STACK_SIDEBAR_H_
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define XAPP_TYPE_STACK_SIDEBAR (xapp_stack_sidebar_get_type ())
+
+G_DECLARE_FINAL_TYPE (XAppStackSidebar, xapp_stack_sidebar, XAPP, STACK_SIDEBAR, GtkBin)
+
+XAppStackSidebar *xapp_stack_sidebar_new (GtkOrientation orientation);
+
+void xapp_stack_sidebar_set_stack (XAppStackSidebar *sidebar,
+                                   GtkStack         *stack);
+
+GtkStack *xapp_stack_sidebar_get_stack (XAppStackSidebar *sidebar);
+
+G_END_DECLS
+
+#endif /*_XAPP_STACK_SIDEBAR_H_ */
