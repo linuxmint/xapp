b9979088203a8be7959faf8fdfebe8d8c1ce6b67
Michael Webster <miketwebster@gmail.com>
2020-12-01 09:33:27 -0500

    xapp-gtk3-module.c: Override GObject's constructed vfunc to inject the favorites uri into GtkPlacesSidebars rather than hooking onto a signal.
    This is much cleaner, and has the bonus of affecting more than just
    file dialogs.

--- libxapp/xapp-gtk3-module.c
+++ libxapp/xapp-gtk3-module.c
@@ -9,72 +9,41 @@
  * The sole purpose of this module currently is to add a 'Favorites'
  * shortcut to GtkFileChooser dialogs.
  *
- * In gtk_module_init, the XAppFavorites singleton is initialized, and
- * the 'favorites' uri scheme is added to the default vfs. Ordinarily
- * non-file:// schemes aren't supported in these dialogs unless their
- * 'local-only' property is set to FALSE. Since favorites are shortcuts
- * to locally-available files, we lie to the chooser setup by returning
- * "/" instead of NULL when g_file_get_path ("favorites:///") is called.
+ * In gtk_module_init, the 'favorites' uri scheme is added to the default
+ * vfs. Ordinarily, non-file:// schemes aren't supported in these dialogs
+ * unless their 'local-only' property is set to FALSE. Since favorites
+ * are shortcuts to locally-available files, we lie to the chooser setup
+ * by returning "/" instead of NULL when g_file_get_path ("favorites:///")
+ * is called.
  */
 
-
-/* Make sure GCC doesn't warn us about a missing prototype for this
- * exported function */
 void gtk_module_init (gint *argc, gchar ***argv[]);
-
-static gboolean
-selection_changed_cb (GSignalInvocationHint *ihint,
-            guint                  n_param_values,
-            const GValue          *param_values,
-            gpointer               data)
-{
-    GtkFileChooser *chooser = GTK_FILE_CHOOSER (g_value_get_object (&param_values[0]));
-    GSList *list, *i;
-    gboolean already_applied = FALSE;
-    list = gtk_file_chooser_list_shortcut_folder_uris (chooser);
-
-    for (i = list; i != NULL; i = i->next)
-    {
-        if (g_strcmp0 ((gchar *) i->data, "favorites:///") == 0)
-        {
-            already_applied = TRUE;
-            break;
-        }
-    }
-
-    g_slist_free_full (list, g_free);
-
-    if (!already_applied)
-    {
-        gtk_file_chooser_add_shortcut_folder_uri (chooser, "favorites:///", NULL);
-    }
-
-    return TRUE;
-}
+static void (* original_sidebar_constructed) (GObject *object);
 
 static void
-add_chooser_hook (GType type)
+xapp_sidebar_constructed (GObject *object)
 {
-    GTypeClass *type_class;
-    guint sigid;
+    GtkPlacesSidebar *sidebar = GTK_PLACES_SIDEBAR (object);
 
-    type_class = g_type_class_ref (type);
+    (* original_sidebar_constructed) (object);
 
-    sigid = g_signal_lookup ("selection-changed", type);
-    g_signal_add_emission_hook (sigid, 0, selection_changed_cb, NULL, NULL);
-
-    g_type_class_unref (type_class);
+    GFile *favorites = g_file_new_for_uri ("favorites:///");
+    gtk_places_sidebar_add_shortcut (sidebar, favorites);
+    g_object_unref (favorites);
 }
 
 G_MODULE_EXPORT void gtk_module_init (gint *argc, gchar ***argv[]) {
+    GObjectClass *object_class;
+
     // This won't instantiate XAppFavorites but will register the uri so
     // it can be used by apps (like pix which doesn't use the favorites api,
     // but just adds favorites:/// to its sidebar.)
     init_favorite_vfs ();
 
-    add_chooser_hook (GTK_TYPE_FILE_CHOOSER_WIDGET);
-    add_chooser_hook (GTK_TYPE_FILE_CHOOSER_DIALOG);
-    add_chooser_hook (GTK_TYPE_FILE_CHOOSER_BUTTON);
+    object_class = g_type_class_ref (GTK_TYPE_PLACES_SIDEBAR);
+
+    original_sidebar_constructed = object_class->constructed;
+    object_class->constructed = xapp_sidebar_constructed;
 }
 
 G_MODULE_EXPORT gchar* g_module_check_init (GModule *module);
