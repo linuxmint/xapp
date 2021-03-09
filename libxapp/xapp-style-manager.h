886b43db893f317ef56786c7e02962b328775b5d
Stephen Collins <scrivera64@gmail.com>
2021-03-08 17:03:58 -0700

    Add a class to programmatically set styling on a particular widget.
    Usage
    -----
    
    c:
    ```
    GtkWidget *widget = gtk_label_new ("blah");
    XAppStyleManager *style_manager = xapp_style_manager_new ();
    xapp_style_manager_set_widget (style_manager, widget);
    xapp_style_manager_set_style_property (style_manager, "color", "red");
    ```
    
    Python:
    
    ```
    widget = Gtk.Label(label="blah")
    style_manager = XApp.StyleManager(widget=widget)
    style_manager.set_style_property("color", "red")
    ```

---
+++ libxapp/xapp-style-manager.h
@@ -0,0 +1,24 @@
+#ifndef __XAPP_STYLE_MANAGER_H__
+#define __XAPP_STYLE_MANAGER_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define XAPP_TYPE_STYLE_MANAGER xapp_style_manager_get_type ()
+G_DECLARE_FINAL_TYPE (XAppStyleManager, xapp_style_manager, XAPP, STYLE_MANAGER, GObject)
+
+XAppStyleManager  *xapp_style_manager_new                   (void);
+
+GtkWidget         *xapp_style_manager_get_widget            (XAppStyleManager *style_manager);
+void               xapp_style_manager_set_widget            (XAppStyleManager *style_manager,
+                                                             GtkWidget        *widget);
+void               xapp_style_manager_set_style_property    (XAppStyleManager *style_manager,
+                                                             const gchar      *name,
+                                                             const gchar      *value);
+void               xapp_style_manager_remove_style_property (XAppStyleManager *style_manager,
+                                                             const gchar      *name);
+
+G_END_DECLS
+
+#endif  /* __XAPP_STYLE_MANAGER_H__  */
